f214be420530dfa366d589b85f11a6eadd7858f0
nicolaskiprop <yegonicolas@gmail.com>
2023-06-01 18:26:25 +0200

    exercise 6

--- exercise06/hadoop_docker_example/hadoop_code/mapper.py
+++ exercise06/hadoop_docker_example/hadoop_code/mapper.py
@@ -1,66 +1,59 @@
 import re
 import sys
-
+from collections import defaultdict
 
 stopwords = [
-    "a",
-    "an",
-    "and",
-    "as",
-    "at",
-    "be",
-    "by",
-    "for",
-    "from",
-    "has",
-    "he",
-    "in",
-    "is",
-    "it",
-    "its",
-    "of",
-    "on",
-    "that",
-    "the",
-    "to",
-    "was",
-    "were",
-    "with",
-    "I",
-    "you",
-    "your",
-    "we",
-    "they",
-    "his",
-    "her",
-    "him",
-    "she",
-    "me",
-    "myself",
-    "ourselves",
-    "them",
-    "themselves",
-    "ours",
-    "our",
-    "who",
-    "what",
-    "where",
-    "when",
-    "why",
-    "how",
-    "which",
-    "there",
-    "here"
+    "a", "an", "and", "as", "at", "be", "by", "for", "from", "has", "he", "in", "is",
+    "it", "its", "of", "on", "that", "the", "to", "was", "were", "with", "I", "you",
+    "your", "we", "they", "his", "her", "him", "she", "me", "myself", "ourselves",
+    "them", "themselves", "ours", "our", "who", "what", "where", "when", "why", "how",
+    "which", "there", "here"
 ]
 
+linksCount = defaultdict(set)
 for line in sys.stdin:
     line = line.strip()
+    line = line.lower()
     line = re.sub(pattern='\W', repl=' ', string=line)
+    line = re.sub(pattern='\s+', repl=' ', string=line)
     words = line.split()
-    for word in words:
-        word = word.lower()
-        if word not in stopwords:
-            print('%s\t%s' % (word, 1))
- 
+    filteredWords = [word for word in words if word not in stopwords]
+    line= ' '.join(filteredWords)
 
-   
+    if not line:
+        continue
+
+    tokens = line.split()
+    for n in range(1, len(tokens) + 1):
+        numTokens = len(tokens)
+        for i in range(numTokens - n + 1):
+            wordTuple = tuple(tokens[i:i+n])
+            sourceWord = wordTuple[0]
+            linksCount[sourceWord].update(wordTuple[1:])
+
+for sourceWord in linksCount:
+    numLinks = len(linksCount[sourceWord])
+    print(f'{sourceWord}\t{numLinks}')
+
+
+#part 1
+# import re
+# import sys
+
+# stopwords = [
+#     "a", "an", "and", "as", "at", "be", "by", "for", "from", "has", "he", "in", "is",
+#     "it", "its", "of", "on", "that", "the", "to", "was", "were", "with", "I", "you",
+#     "your", "we", "they", "his", "her", "him", "she", "me", "myself", "ourselves",
+#     "them", "themselves", "ours", "our", "who", "what", "where", "when", "why", "how",
+#     "which", "there", "here"
+# ]
+
+# for line in sys.stdin:
+#     line = line.strip()
+#     line = line.lower()
+#     line = re.sub(pattern='\W', repl=' ', string=line)
+#     line = re.sub(pattern='\s+', repl=' ', string=line)
+#     words = line.split()
+#     filteredWords = [word for word in words if word not in stopwords]
+#     line = ' '.join(filteredWords)
+#     print(line)
--- exercise06/hadoop_docker_example/hadoop_code/reducer.py
+++ exercise06/hadoop_docker_example/hadoop_code/reducer.py
@@ -1,25 +1,18 @@
 import sys
-
-current_word = None
-current_count = 0
-word = None
+treshold = 100
+linkCounter = 0
 
 for line in sys.stdin:
-    line = line.strip()
-    word, count = line.split('\t', 1)
+    word, count = line.strip().split('\t')
+    count = int(count)
+    if count > treshold:
+        print(f'{word}\t{count}')
 
-    try:
-        count = int(count)
-    except ValueError:
-        continue
 
-    if current_word == word:
-        current_count += count
-    else:
-        if current_word:
-            print ('%s\t%s' % (current_word, current_count))
-        current_count = count
-        current_word = word
+#     linkCounter += 1
+# print(f'Number of links: {linkCounter}')
 
-if current_word == word:
-    print ('%s\t%s' % (current_word, current_count))
+# import sys
+# for line in sys.stdin:
+#     line = line.strip()
+#     print(line)
