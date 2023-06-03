9a4a23ad9ba041bcdf05c95941fd210c8b1f9678
nicolaskiprop <yegonicolas@gmail.com>
2023-06-03 10:45:59 +0200

    using dot product

--- exercise2/dotproduct.py
+++ exercise2/dotproduct.py
@@ -1,12 +1,15 @@
 #dot product using for loop
 import sys
+import numpy as np
 
 A = [1, 2, 3]
 B = [4, 5, 6]
 
-dot_product = 0
-for i in range(len(A)):
-    dot_product += A[i] * B[i]
+# dot_product = 0
+# for i in range(len(A)):
+#     dot_product += A[i] * B[i]
 
-print(dot_product)
+# print(dot_product)
 
+result=np.dot(A, B)
+print(result)
